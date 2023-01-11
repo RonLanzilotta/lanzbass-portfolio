f82698efdc0827db2060960d61f77a3cbee16861
Ron Lanzilotta <rlanzilotta1010@gmail.com>
2023-01-11 11:45:09 -0500

    I can live with this

--- src/Entry.jsx
+++ src/Entry.jsx
@@ -1,30 +1,32 @@
-
-import Modal from './Modal.jsx'
-import { useState } from 'react'
+import Modal from "./Modal.jsx";
+import { useState } from "react";
 
 export default function Entry({ entries }) {
+  const [toggle, setToggle] = useState(false);
+  const [displayItem, setDisplayItem] = useState({});
 
-    const [toggle, setToggle] = useState(false)
-    const [displayItem, setDisplayItem] = useState({})
-
-    const display = (entry) => {
-        if (!toggle) return setToggle(true), setDisplayItem(entry);
-        return setToggle(false);
-      };
+  const display = (entry) => {
+    if (!toggle) return setToggle(true), setDisplayItem(entry);
+    return setToggle(false);
+  };
 
   return (
     <div className="app">
-        {toggle && <Modal entry={displayItem} display={display} />}
-        {entries.map((entry, index) => {
-            if (entry.display) {
-            return (
-            <img 
-                onClick={() => display(entry)}
-                className="image" 
-                src={entry.img} 
-                key={index}
-        />
-)}})}
+      {toggle && (
+          <Modal entry={displayItem} display={display} />
+      )}
+      {entries.map((entry, index) => {
+        if (entry.display) {
+          return (
+            <img
+              onClick={() => display(entry)}
+              className="image"
+              src={entry.img}
+              key={index}
+            />
+          );
+        }
+      })}
     </div>
   );
 }
--- src/Modal.jsx
+++ src/Modal.jsx
@@ -1,19 +1,25 @@
-import Entry from "./Entry";
-
 export default function Modal({ entry, display }) {
+  //   const arrFixer = (arr) => {
+  //     for (let i = 0; i < arr.length; i++) {
+  //       arr[i] = arr[i] + ", ";
+  //     }
+  //     return arr;
+  //   };
+
+  console.log(entry.songs.join(", "));
 
   return (
     <div className="modal" onClick={display}>
-        <img src={entry.img} className="modalImg"></img>
-        <ul>
-            <li>Artist: {entry.artist}</li>
-            <li>Album: {entry.album}</li>
-            <li>Year: {entry.artist}</li>
-            <li>Songs: {entry.songs}</li>
-            <li>Roles: {entry.artist}</li>
-            <li>Record Label: {entry.artist}</li>
-            <li>Link: {entry.artist}</li>
-        </ul>
+      <img src={entry.img} className="modalImg"></img>
+      <ul>
+        <li>Artist: {entry.artist}</li>
+        <li>Album: {entry.album}</li>
+        <li>Year: {entry.artist}</li>
+        <li>Songs: {entry.songs.join(", ")}</li>
+        <li>Roles: {entry.roles.join(", ")}</li>
+        <li>Record Label: {entry.recordLabel}</li>
+        <li><a href={entry.link}>Listen</a></li>
+      </ul>
     </div>
-  )
+  );
 }
